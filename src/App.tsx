30b616921f1da78fa447ba8e5ebbce8a52cd538a
felociouske <felociouske@gmail.com>
2026-01-02 13:45:14 +0300

    removed the install extension component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/layouts/Navbar";
 import Footer from './components/layouts/Footer';
 import FloatingWhatsApp from './components/sections/FloatingButton';
-import InstallPrompt from './components/sections/InstallPrompt';
 import Home from './pages/Home';
 import About from './pages/About';
 
@@ -24,7 +23,6 @@ const App: React.FC = () => {
           message="Hello! I would like to know more about LexiQuick."
           position="right"
         />
-        <InstallPrompt extensionUrl="https://chrome.google.com/webstore/detail/your-extension-id" />
       </div>
     </Router>
   );
