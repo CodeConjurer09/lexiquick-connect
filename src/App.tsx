1262dd682828ed9d94a6e4a7e37a5f14ee5e877c
felociouske <felociouske@gmail.com>
2026-04-29 17:03:11 +0300

    Removed all the pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from "./components/layouts/Navbar";
-import Footer from './components/layouts/Footer';
-import FloatingWhatsApp from './components/sections/FloatingButton';
-import Home from './pages/Home';
-import About from './pages/About';
-import ProofPage from './pages/ProofPage';
+import CautionPage from "./pages/Caution";
 
-const App: React.FC = () => {
+function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-white flex flex-col">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/successful-transactions" element={<ProofPage />} />
-          </Routes>
-        </main>
-        <Footer />
-        <FloatingWhatsApp 
-          phoneNumber="254707569122"
-          message="Hello! I would like to know more about LexiQuick."
-          position="right"
-        />
-      </div>
-    </Router>
+    <div>
+      <CautionPage />
+    </div>
   );
-};
+}
 
 export default App;
---
+++ src/pages/Caution.tsx
@@ -0,0 +1,11 @@
+export default function CautionPage() {
+
+  return (
+    <div className="min-h-screen bg-black text-red-500 flex flex-col items-center px-6 py-12">
+      
+      <h1 className="text-5xl md:text-7xl font-extrabold tracking-widest mb-12 animate-pulse drop-shadow-[0_0_15px_red]">
+        ⚠️ ACCESS DENIED ⚠️
+      </h1>
+    </div>
+  );
+}
